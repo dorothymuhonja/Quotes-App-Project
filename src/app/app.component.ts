f4a9bbdf46f13ec2c09862fe386b5fec5775fccb
dorothymuhonja <dorothymuhonja7@gmail.com>
2021-01-29 12:19:53 +0300

    add function for displaying quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes-Project';
+ quotes: Quote[] = [
+   {id:1, name:`Hope is like a broken pen, wont work, but you hold on to it since it is the only pen you've got`},
+   {id:2, name: `I see unconditional love when I look at my mother`},
+   {id:3, name: `Knowledge is power is time is money`},
+   {id:4, name:`God gives us ugliness so we don't take the beautiful things in life fot granted`}
+ ]
+
 }
